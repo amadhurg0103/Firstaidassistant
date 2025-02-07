766c07025bbbb9a9a9976287a36da62fa84d0564
amadhurg0103 <a.madhurg.n0103@gmail.com>
2025-02-07 23:02:32 +0530

    intial commit

---
+++ FirstAidAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAidAssistantApp.swift
+//  FirstAidAssistant
+//
+//  Created by Madhur Gupta on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAidAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
